b2efacac4a983a0dd9b4af106353a458c2b4941b
cooler-SAI <coolerme86@gmail.com>
2025-06-07 20:30:56 +0300

    feat: Demonstrate basic goroutine and WaitGroup usage
    This commit introduces a simple Go program to illustrate the
    use of goroutines for concurrent execution and `sync.WaitGroup`
    for synchronization.
    
    Key features:
    - A `hello` function is defined to print a greeting, designed
      to be run as a goroutine and signal completion via `sync.WaitGroup`.
    - The `main` function launches two goroutines:
      1. An anonymous goroutine that prints a message.
      2. The `hello` function with a specific name.
    - `sync.WaitGroup` is used to ensure the `main` function waits for
      both goroutines to complete before printing "Done" and exiting.

--- test/test.go
+++ test/test.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+func hello(name string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Hello", name)
+}
 
 func main() {
 	fmt.Println("hello world")
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("hello world NEW")
+	}()
+
+	wg.Add(1)
+
+	go hello("Ander", &wg)
+	wg.Wait()
+	fmt.Println("Done")
+
 }
